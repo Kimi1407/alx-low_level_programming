78478cb5b4570bcf833ffe39ba267f42b9fd4ac1
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-08 14:12:30 -0700

    task 4 check

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * code should print "Programming is like building a multilingual puzzle if correct
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	returns (0);
+}
