4200ea21f0732442ba82fac14c6b891bd9b016b2
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-09 14:40:00 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - print alphabets in lower case
+ * Description: using the putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; c++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
+
