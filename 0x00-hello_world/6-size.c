5730b0a3892ae777828b9643dcfe0419f53d446a
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-08 19:34:23 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,17 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an init: %d byte(s)\n", sizeof(init));
-	printf("Size of a long init %d byte(s)\n", sizeof(long init));
-	printf("Size of a long long init %d byte(s)\n", sizeof(long long init));
-	printf("Size of a float %d byte(s)\n", sizeof(float));
+	char age;
+	int aged;
+	long int ages;
+	long long int ag;
+	float agem;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(age));
+	printf("Size of an init: %d byte(s)\n", sizeof(aged));
+	printf("Size of a long init %d byte(s)\n", sizeof(long ages));
+	printf("Size of a long long init %d byte(s)\n", sizeof(long long ag));
+	printf("Size of a float %d byte(s)\n", sizeof(agem));
 	return (0);
 }
 
