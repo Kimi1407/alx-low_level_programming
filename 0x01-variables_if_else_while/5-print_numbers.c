008feed93d5c7711878920d4cb43794cc76f5749
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-09 16:58:20 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,15 +7,13 @@
  */
 int main(void)
 {
-        char n;
+	char n;
 
-        for (n = '0'; n <= '9'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-                putchar(n);
+		putchar(n);
 	}
-
-        putchar('\n');
-        
+	putchar('\n');
 	return (0);
 }
 
