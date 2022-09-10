9de1d2d6076bf1e82aa9fbf47888d8ccdfda1558
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-09 17:22:38 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Description: using the putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+	for (n = 'z'; n <= 'a'; n--)
+	{
+		putchar(n);
+	}
+
+	putchar("\n");
+	return (0);
