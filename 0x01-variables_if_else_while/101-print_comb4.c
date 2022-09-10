081d474925a01f90ae1b2a905e75e8b9c19062d5
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-09 19:16:04 -0700

    optional 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '25'; a <= '34'; a++)
+	for (a = '25'; a < 34'; a++)
 	{
-		for (b = '26'; b <= '34'; b++)
+		for (b = '26'; b < '34'; b++)
 		{
-			for (c = '27'; c <= '34'; c++)
+			for (c = '27'; c < '34'; c++)
 			{
 				if (c > b && b > a)
 				{
