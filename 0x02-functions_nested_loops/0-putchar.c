f84eac0fd18ded757db08c2b3e5189ffc249b8a9
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-13 09:07:03 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints _putchar
+ * Description: using the putchar function
+ * RETURN: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '_putchar';)
+	
+		putchar(c);
+	
+	putchar("\n");
+	return (0);
+}
