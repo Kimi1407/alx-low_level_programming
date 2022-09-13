cfbe3b3a5f2a9b70b2409d0fffdb4b061ec70c45
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-13 11:07:03 -0700

    commit two

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints 10 times the alphabet, in lowercase
+ * Description: using _putchar
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c, a;
+	for (i = 0; i <=9 i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
