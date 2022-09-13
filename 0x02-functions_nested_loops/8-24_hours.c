1769ac492bd730146908a99620102666724e40c1
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-13 16:45:18 -0700

    commit seven

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,10 +21,11 @@ void jack_bauer(void)
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
 		}
+		
 		i++;
 	}
 }
