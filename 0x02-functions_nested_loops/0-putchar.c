0d0daecc2b8fc2b5112224a3c3ad335285057ec6
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-13 10:19:59 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,17 @@
  * Description: using the putchar function
  * RETURN: 0
  */
+
 int main(void)
 {
-	char c;
-
-	for (c = '_putchar';)
-	
-		putchar(c);
-	
-	putchar("\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
