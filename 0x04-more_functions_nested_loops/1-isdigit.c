c987b15a11248441ffaed3aab185ccb5e50d77bb
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-15 16:57:16 -0700

    task 1 and 3

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,17 @@
-#include "main.h"
+#include "main"
 
 /**
- * _isdigit - checks for a digit 0 through 9
+ * _isdigit -  checks for a digit 0 through 9
  * @c: tested character
- * Return: 1 if tested 0 if not
+ * Return: 0
  */
 
 int _isdigit(int c)
 {
-	if ((c >= 0) && (c >= 9))
+	if ((c >= 0) && (c <= 9))
 	{
 		return (1);
 	}
 	else
-	{
 		return (0);
-	}
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers
- * from 0 to 9
- * Return: returns nothing
+ * print_numbers - prints the numbers, from
+ * 0 to 9
+ * Return: 0
  */
 
 void print_numbers(void)
 {
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		putchar(n);
 	}
