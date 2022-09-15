7a42dfb7053bc817f8f9613f63f94e74cfbd5fb5
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-15 10:55:59 -0700

    commit 4

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,12 +2,12 @@
 
 /**
  * print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -32,5 +32,6 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-	}
+	
+	retu}
 }
