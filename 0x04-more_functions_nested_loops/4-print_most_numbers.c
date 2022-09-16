71104080f9f14fc7f590b721789d83004961d429
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-15 17:09:10 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers except 2 and 4
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if ((n == 2) || (n == 4))
+		{
+			continue;
+		}
+		putchar(n);
+	}
+	putchar(10);
+}
+
