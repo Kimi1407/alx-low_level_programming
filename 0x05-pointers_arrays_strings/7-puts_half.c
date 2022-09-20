2f6b938798e63970a2eb9d8734240d294ad80821
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-19 18:11:39 -0700

    task 8 and 9

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,14 +21,14 @@ void puts_half(char *str)
 	{
 		for (b = len / 2; str[b] != '\0'; b++)
 		{
-			_putchar(str[b]);
+			putchar(str[b]);
 		}
 	} else if (len % 2)
 	{
 		for (a = (len - 1) / 2; a < len - 1; a++)
 		{
-			_putchar(str[a + 1]);
+			putchar(str[a + 1]);
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
