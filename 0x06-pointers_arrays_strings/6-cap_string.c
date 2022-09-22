523bf575bb4c73e31bdf3ea51faf048cd43d05e1
Kimi1407 <salamander.waffles1407@gmail.com>
2022-09-21 17:43:22 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length as an integer
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @str: string to be capitalized
+ * Return: capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[++index])
+	{
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
+	}
+
+	return (str);
+}
+
+
